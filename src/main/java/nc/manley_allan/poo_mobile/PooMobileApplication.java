eb79ef6afe911205b5dc507d4751e103b326a306
Manley.B <manley-bouille@outlook.fr>
2026-09-02 15:42:05 +1100

    init

---
+++ src/main/java/nc/manley_allan/poo_mobile/PooMobileApplication.java
@@ -0,0 +1,13 @@
+package nc.manley_allan.poo_mobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PooMobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PooMobileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nc/manley_allan/poo_mobile/PooMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package nc.manley_allan.poo_mobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PooMobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

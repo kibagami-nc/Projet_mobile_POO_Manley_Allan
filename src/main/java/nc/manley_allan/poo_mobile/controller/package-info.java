2dcc704932c5d3fffcaf5502db30e2108a2915a9
Manley.B <manley-bouille@outlook.fr>
2026-09-23 13:22:14 +1100

    feat(payload): Adding login payload (R)

--- src/main/java/nc/manley_allan/poo_mobile/controller/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * API REST exposée à l'application Flutter : classes annotées
- * {@code @RestController}, une par ressource (lieux, catégories, avis...).
- */
-package nc.manley_allan.poo_mobile.controller;
